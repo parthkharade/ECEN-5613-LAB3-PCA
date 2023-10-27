826fd73744e7ca7935def3c0970512dd59981d77
Parth <parthkharade99@gmail.com>
2023-10-27 12:32:27 -0600

    Added comments to core.h and pca.h

--- Inc/core.h
+++ Inc/core.h
@@ -1,7 +1,31 @@
 #include "at89c51ed2.h"
 #include "stdint.h"
+
+/**
+ * @brief Initialises the UART to use the Internal BRG and sets the baud rate to 57600.
+ * 
+ */
 void init_uart();
+
+/**
+ * @brief This function is called when Fclk is at minimum. This sets the UART baud rate to 1355bps
+ * 
+ */
 void init_uart_slow();
+
+/**
+ * @brief Initalises the clock in X2 mode and at full frequency.
+ * 
+ */
 void init_clocks();
+/**
+ * @brief Intialises the INT0 as a falling edge trigger.
+ * 
+ */
 void init_INT0();
+
+/**
+ * @brief Resets the watchdog timer.
+ * 
+ */
 void reset_WDT();
--- Inc/pca.h
+++ Inc/pca.h
@@ -1,7 +1,39 @@
+/**
+ * @brief Initialise the PCA to use Fclk/2 and enable WDT,PWM and Highspeed output mode
+ * 
+ */
 void init_pca();
+/**
+ * @brief Starts the pwm module of the PCA
+ * 
+ */
 void start_pwm();
+
+/**
+ * @brief Stops the pwm module of the PCA
+ * 
+ */
 void stop_pwm();
+/**
+ * @brief Starts the High speed output module of the PCA
+ * 
+ */
 void start_tog();
+
+/**
+ * @brief Stops the high speed output module of the pca
+ * 
+ */
 void stop_tog();
+
+/**
+ * @brief Enable the WDT
+ * 
+ */
 void enable_dog();
+
+/**
+ * @brief Disable the WDT
+ * 
+ */
 void disable_dog();
